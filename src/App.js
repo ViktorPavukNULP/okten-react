5b244c446483027ea8ad5966b0642c9c66d17a08
MekolaM <viktorpavuk87@gmail.com>
2022-01-24 17:38:52 +0200

    rick and morty setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Layout from "./components/Layout/Layout";
 
 function App() {
-  return (
-    <div className="App">
-      Hello world
-    </div>
-  );
+    return (
+        <div className="App">
+            <Layout/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Layout = () => {
+    return (
+        <div>
+            Rick and Morty
+        </div>
+    );
+};
+
+export default Layout;
