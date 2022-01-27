38a5f30f5b0fe648c53ae03011d7492e60ca577a
ViktorPavukNULP <72486611+ViktorPavukNULP@users.noreply.github.com>
2022-01-27 18:53:22 +0200

    classwork 4 setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import "./App.css";
+import {useReducer} from "react";
+
+const reducer = (state, action) => {
+  console.log("ggg")
+}
 
 function App() {
-  return (
-    <div className="App">
-      Hello world
-    </div>
-  );
+    const [state, dispatch] = useReducer(reducer, {cats: [], dogs: []});
+    return (
+        <div className="App">
+            <div className="Header">
+                Add Cat
+                <input type="text"/>
+                <button onClick={()=>dispatch()}>Save</button>
+                Add Dog
+                <input type="text"/>
+                <button onClick={()=>dispatch()}>Save</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
