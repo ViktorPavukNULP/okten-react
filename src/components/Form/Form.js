f11b5780992356853f324cd370287eb841f03b43
ViktorPavukNULP <72486611+ViktorPavukNULP@users.noreply.github.com>
2022-01-31 12:33:00 +0200

    homework 5 redux joi validator

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,11 +1,13 @@
 import React, {useEffect} from "react";
 import {useForm} from "react-hook-form";
 import {useDispatch, useSelector} from "react-redux";
+import {joiResolver} from "@hookform/resolvers/joi";
 
 import {createCar, updateCar} from "../../store/car.slice";
+import {CarValidator} from "../../validators/car.validator";
 
 const Form = () => {
-    const {handleSubmit, register, reset, setValue} = useForm();
+    const {handleSubmit, register, reset, setValue, formState: {errors}} = useForm({resolver: joiResolver(CarValidator), mode: "onTouched"});
     const dispatch = useDispatch();
     const {carForUpdate} = useSelector(state => state["carReducer"]);
 
@@ -24,10 +26,15 @@ const Form = () => {
     }
     return (
         <form onSubmit={handleSubmit}>
-            <label>Model: <input type="text" {...register('model')}/></label>
-            <label>Price: <input type="text" {...register('price')}/></label>
-            <label>Year: <input type="text" {...register('year')}/></label>
-            <button onClick={handleSubmit(submit)}>Create</button>
+            <div><label>Model: <input type="text" {...register('model')}/></label></div>
+            {errors.model && <span>{errors.model.message}</span>}
+            <div><label>Price: <input type="text" {...register('price')}/></label></div>
+            {errors.price && <span>{errors.price.message}</span>}
+            <div><label>Year: <input type="text" {...register('year')}/></label></div>
+            {errors.year && <span>{errors.year.message}</span>}
+            <div>
+                <button onClick={handleSubmit(submit)}>Create</button>
+            </div>
             {carForUpdate.id && <button onClick={handleSubmit(update)}>Update</button>}
         </form>
     );
---
+++ src/validators/car.validator.js
@@ -0,0 +1,11 @@
+import Joi from "joi";
+
+
+export const CarValidator = Joi.object({
+    model: Joi.string().regex(new RegExp('^[[A-Za-zА-яіїІЇёЁ]{1,20}$')).required()
+        .messages({
+        'string.pattern.base': "Only letters"
+    }),
+    price: Joi.number().min(0).max(100000).required(),
+    year: Joi.number().min(1990).max(new Date().getFullYear()).required(),
+});
