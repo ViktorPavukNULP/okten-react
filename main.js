1b4dc74ad4af33a48601abf40cb490cb9d10857c
ViktorPavukNULP <72486611+ViktorPavukNULP@users.noreply.github.com>
2022-02-03 18:18:58 +0200

    homework-6

---
+++ main.js
@@ -0,0 +1,59 @@
+"use strict";
+// 1) создать интерфейс на основе этого объекта:
+exports.__esModule = true;
+var launch1 = {
+    "mission_name": "Starlink-15 (v1.0)",
+    "launch_date_local": "2020-10-24T11:31:00-04:00",
+    "launch_site": {
+        "site_name_long": "Cape Canaveral Air Force Station Space Launch Complex 40"
+    },
+    "links": {
+        "article_link": "some link",
+        "video_link": "https://youtu.be/J442-ti-Dhg"
+    },
+    "rocket": {
+        "rocket_name": "Falcon 9",
+        "first_stage": {
+            "cores": [
+                {
+                    "flight": 7,
+                    "core": {
+                        "reuse_count": 6,
+                        "status": "unknown"
+                    }
+                }
+            ]
+        },
+        "second_stage": {
+            "payloads": [
+                {
+                    "payload_type": "Satellite",
+                    "payload_mass_kg": 15400,
+                    "payload_mass_lbs": 33951.2
+                }
+            ]
+        }
+    }
+};
+console.log(launch1.rocket);
+// 2) протипизировать функции:
+//
+var user = {
+    name: "Max",
+    age: 18,
+    gender: 'male'
+};
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(1, 2));
+function showSum(a, b) {
+    console.log(a + b);
+}
+showSum(4, 2);
+function incAge(someUser, inc) {
+    someUser.age += inc;
+    return someUser;
+}
+console.log(incAge(user, 2));
+console.log(incAge({ name: "Andriy", age: 12, gender: "male", wife: "Stefa" }, 2));
